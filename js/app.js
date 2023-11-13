9255ee4fb87915a1a28990db118ba707dbfd29c6
mashrurmahmud <101925709+mashrurmahmud@users.noreply.github.com>
2023-11-13 06:01:26 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,22 @@
+
+
+
+
+ document.querySelector('#open').addEventListener('click',()=>{
+    
+    if (document.querySelector('#search').classList.contains('show')){
+      document.querySelector('#search').classList.add('remova')
+
+    }else{
+      document.querySelector('#search').classList.add('show');
+    }
+      
+     
+
+        
+   
+   
+
+})
+
+
